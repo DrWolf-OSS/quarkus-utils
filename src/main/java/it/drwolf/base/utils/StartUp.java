ede3c7a4a8d7ab69eafbae100929f42ae3779c4a
Andrea Agili <agea@drwolf.it>
2023-10-12 10:35:49 +0200

    :loud_sound: :rocket: 3.4.2.2

--- src/main/java/it/drwolf/base/utils/StartUp.java
+++ src/main/java/it/drwolf/base/utils/StartUp.java
@@ -18,6 +18,11 @@ public class StartUp implements HasLogger {
 		TimeZone.setDefault(TimeZone.getTimeZone(tz));
 		Locale.setDefault(Locale.forLanguageTag(locale));
 		logger().infof("Timezone set to %s", tz);
+		logger().infof("Java version: %s", Runtime.version());
+		logger().infof("Processors: %d", Runtime.getRuntime().availableProcessors());
+		logger().infof("Total Memory: %d MB", Runtime.getRuntime().totalMemory() / 1048576);
+		logger().infof("Max Memory: %d MB", Runtime.getRuntime().maxMemory() / 1048576);
+		logger().infof("Free Memory: %d MB", Runtime.getRuntime().freeMemory() / 1048576);
 	}
 
 }

5f25a2ef6a99d0d39854b148d76b89bbf027ffef
Andrea Agili <agea@drwolf.it>
2024-07-03 14:50:41 +0200

    :mute:

--- src/main/java/it/drwolf/base/utils/CustomExceptionHandler.java
+++ src/main/java/it/drwolf/base/utils/CustomExceptionHandler.java
@@ -1,22 +1,24 @@
 package it.drwolf.base.utils;
 
-import io.vertx.core.http.HttpServerRequest;
-import jakarta.ws.rs.WebApplicationException;
-import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.ext.ExceptionMapper;
-import jakarta.ws.rs.ext.Provider;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-import org.jboss.resteasy.spi.ResteasyProviderFactory;
-
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.resteasy.spi.ResteasyProviderFactory;
+
+import io.vertx.core.http.HttpServerRequest;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
 @Provider
 public class CustomExceptionHandler implements ExceptionMapper<Exception>, HasLogger {
 
-	@ConfigProperty(name = "quarkus-utils.stack-trace-limit", defaultValue = "20")
+	@ConfigProperty(name = "quarkus-utils.stack-trace-limit",
+			defaultValue = "20")
 	Integer limit;
 
 	@Override
@@ -27,9 +29,14 @@ public class CustomExceptionHandler implements ExceptionMapper<Exception>, HasLo
 
 		StringWriter sw = new StringWriter();
 		e.printStackTrace(new PrintWriter(sw));
-		this.logger()
-				.error(Stream.concat(Stream.of(request.method() + " " + request.uri()),
-						sw.toString().lines().limit(limit)).collect(Collectors.joining("\n")));
+
+		String logTxt = sw.toString();
+
+		if (!logTxt.contains("SRJWT07000")) {
+			this.logger()
+					.error(Stream.concat(Stream.of(request.method() + " " + request.uri()), logTxt.lines().limit(limit))
+							.collect(Collectors.joining("\n")));
+		}
 
 		if (e instanceof WebApplicationException wae) {
 			builder = Response.status(wae.getResponse().getStatus());

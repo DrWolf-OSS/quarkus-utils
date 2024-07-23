700e1f66db9087c4b48dc46a07436d58c64b4aab
Alessandro Paperini <alessandro.paperini@drwolf.it>
2024-07-23 11:00:48 +0200

    :bricks: migration to quarkus-rest

--- src/main/java/it/drwolf/base/utils/CustomExceptionHandler.java
+++ src/main/java/it/drwolf/base/utils/CustomExceptionHandler.java
@@ -6,10 +6,10 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
-import org.jboss.resteasy.spi.ResteasyProviderFactory;
 
 import io.vertx.core.http.HttpServerRequest;
 import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
@@ -21,11 +21,12 @@ public class CustomExceptionHandler implements ExceptionMapper<Exception>, HasLo
 			defaultValue = "20")
 	Integer limit;
 
+	@Context
+	HttpServerRequest request;
+
 	@Override
 	public Response toResponse(Exception e) {
 		Response.ResponseBuilder builder;
-
-		HttpServerRequest request = ResteasyProviderFactory.getInstance().getContextData(HttpServerRequest.class);
 
 		StringWriter sw = new StringWriter();
 		e.printStackTrace(new PrintWriter(sw));
@@ -34,7 +35,7 @@ public class CustomExceptionHandler implements ExceptionMapper<Exception>, HasLo
 
 		if (!logTxt.contains("SRJWT07000")) {
 			this.logger()
-					.error(Stream.concat(Stream.of(request.method() + " " + request.uri()), logTxt.lines().limit(limit))
+					.error(Stream.concat(Stream.of(this.request.method() + " " + this.request.uri()), logTxt.lines().limit(limit))
 							.collect(Collectors.joining("\n")));
 		}
 

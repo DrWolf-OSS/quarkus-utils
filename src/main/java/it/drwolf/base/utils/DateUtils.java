c939573ac66779331421035446792589abf10290
Andrea Agili <agea@drwolf.it>
2026-04-16 10:40:15 +0200

    :gial_net: :rocket: 3.33.1.3 date parsing

--- src/main/java/it/drwolf/base/utils/DateUtils.java
+++ src/main/java/it/drwolf/base/utils/DateUtils.java
@@ -1,15 +1,19 @@
 package it.drwolf.base.utils;
 
-import jakarta.ws.rs.BadRequestException;
-
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeParseException;
 import java.util.Date;
 import java.util.Optional;
 
+import jakarta.ws.rs.BadRequestException;
+
 public class DateUtils {
+
 	public static Optional<Date> parseOptional(String isoString) {
 		try {
-			return Optional.ofNullable(isoString).map(s -> Date.from(ZonedDateTime.parse(s).toInstant()));
+			return Optional.ofNullable(isoString).map(DateUtils::parseDate);
 		} catch (Exception e) {
 			throw new BadRequestException(e);
 		}
@@ -17,5 +21,13 @@ public class DateUtils {
 
 	public static Date parse(String isoString) {
 		return parseOptional(isoString).orElseThrow(BadRequestException::new);
+	}
+
+	private static Date parseDate(String isoString) {
+		try {
+			return Date.from(ZonedDateTime.parse(isoString).toInstant());
+		} catch (DateTimeParseException ignored) {
+			return Date.from(LocalDateTime.parse(isoString).toInstant(ZoneOffset.UTC));
+		}
 	}
 }
